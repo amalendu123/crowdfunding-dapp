6d495588fbae0986f35567c83c072ebee15acfe0
AMALENDU MANOJ <220229@tkmce.ac.in>
2024-05-18 12:01:21 +0530

    .

--- Context/context.js
+++ Context/context.js
@@ -1,5 +1,5 @@
 import crowdFunding from "../Context/CrowdFunding.json";    
 
 
-export const CrowdFundingAddress="0x5adEf0aA58D07b27210288965507Ab2bFFfEAcb5";
+export const CrowdFundingAddress="0x1A98EeB1aaB5E81cF93AE4434d54b51BB3288368";
 export const CrowdFundingABI=crowdFunding.abi;
