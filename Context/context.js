ca0170efadf6ce05ddd8f19bdc3c6493bc8668c3
AMALENDU MANOJ <220229@tkmce.ac.in>
2024-05-18 09:39:20 +0530

    updated to sepolia testnet

--- Context/context.js
+++ Context/context.js
@@ -1,5 +1,5 @@
 import crowdFunding from "../Context/CrowdFunding.json";    
 
 
-export const CrowdFundingAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const CrowdFundingAddress="0x5adEf0aA58D07b27210288965507Ab2bFFfEAcb5";
 export const CrowdFundingABI=crowdFunding.abi;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+const INFURA_API_KEY = "3992e4670ba74252864098116d844c36"
+const SEPOLIA_PRIVATE_KEY = "cc04d01d236840eb3ecddd8cb3fa25b20036d4f042845344180b22cb65848e02";
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
